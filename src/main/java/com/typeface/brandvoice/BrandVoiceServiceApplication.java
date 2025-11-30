ae2499dbd8e53a4aaf799e8a77fa5c8c50362c70
satya-supercluster <satya.supercluster@gmail.com>
2025-11-30 16:15:05 +0530

    first commit

---
+++ src/main/java/com/typeface/brandvoice/BrandVoiceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.typeface.brandvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandVoiceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrandVoiceServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/typeface/brandvoice/BrandVoiceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.typeface.brandvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandVoiceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
